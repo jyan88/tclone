062123d5487e57d715872328fc9120202e1adcc2
jyan88 <40650013+jyan88@users.noreply.github.com>
2018-11-25 13:25:43 +0900

    制御再追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update, :show]
   
   def new
     @user = User.new
